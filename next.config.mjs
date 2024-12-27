e66a131e6250ddc4a1a258aa5540c2ee9239d872
veyis <vakgul@gmail.com>
2024-12-27 21:45:32 +0300

    next15

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      NEXT_PUBLIC_COMPANY_NAME: process.env.NEXT_PUBLIC_COMPANY_NAME,
+      NEXT_PUBLIC_CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL,
+      NEXT_PUBLIC_CONTACT_PHONE: process.env.NEXT_PUBLIC_CONTACT_PHONE,
+    },
+    // Add this section to include the sitemap in the output
+    async rewrites() {
+      return [
+        {
+          source: '/sitemap.xml',
+          destination: '/api/sitemap',
+        },
+      ]
+    },
+  }
+  
+  export default nextConfig
